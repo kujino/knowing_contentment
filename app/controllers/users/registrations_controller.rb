7e9adddaeaabbeb7637791e9d4042b4a7a00fb51
kujino <yorunokuji@gmail.com>
2025-11-11 11:30:29 +0900

    add:ユーザー機能のフラッシュメッセージ実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
   protected
 
-  #サインアップ後のリダイレクト先
+  # サインアップ後のリダイレクト先
   def after_sign_up_path_for(resource)
     root_path
   end
