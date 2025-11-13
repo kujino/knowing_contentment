7537cce521df5fd601e1aadf17c444aea1a94fef
kujino <yorunokuji@gmail.com>
2025-11-13 15:18:48 +0900

    add:テーブル編集(themes << title_id, posts << theme_id)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_13_001618) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_13_055944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_13_001618) do
     t.boolean "is_anonymous", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "theme_id", null: false
+    t.index ["theme_id"], name: "index_posts_on_theme_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -28,6 +30,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_13_001618) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "title_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +46,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_13_001618) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "themes"
   add_foreign_key "posts", "users"
 end
