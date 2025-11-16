908a7263050f5eb37cced5e5136d3c721c880b1f
kujino <yorunokuji@gmail.com>
2025-11-16 15:39:01 +0900

    posts#show実装途中辞め

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "mypage" => "mypages#show"
 
-  resources :posts, only: [ :new, :create ]
+  resources :posts, only: [ :new, :create, :show ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
