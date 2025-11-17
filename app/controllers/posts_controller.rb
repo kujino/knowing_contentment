207ed0271ceb51416e4df73897fbe0adb5f1026a
kujino <yorunokuji@gmail.com>
2025-11-17 22:43:53 +0900

    post#show実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :post_show_authorize, only: [:show]
 
   def new
     @post = Post.new
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post
   end 
 
   private
@@ -23,4 +24,11 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:content, :is_anonymous, :theme_id, :image)
   end
+
+  def post_show_authorize
+    @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to root_path, alert: "アクセス権がありません"
+    end
+  end 
 end
