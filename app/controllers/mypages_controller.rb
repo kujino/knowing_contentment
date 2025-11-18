4850f94b7c81a049b39520594880211a533af150
kujino <yorunokuji@gmail.com>
2025-11-18 13:39:36 +0900

    タイムライン機能実装

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,4 @@
 class MypagesController < ApplicationController
-
   def show
     @user = current_user
     @posts = current_user.posts.order(created_at: :desc)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  before_action :post_show_authorize, only: [:show]
+  before_action :post_show_authorize, only: [ :show ]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
 
   def new
     @post = Post.new
@@ -17,7 +21,7 @@ class PostsController < ApplicationController
 
   def show
     @post
-  end 
+  end
 
   private
 
@@ -25,10 +29,11 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content, :is_anonymous, :theme_id, :image)
   end
 
+  # 投稿詳細はポストユーザー以外アクセス不可
   def post_show_authorize
     @post = Post.find(params[:id])
     if @post.user != current_user
       redirect_to root_path, alert: "アクセス権がありません"
     end
-  end 
+  end
 end
