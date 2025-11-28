8aafed71b82496bf4e937920c6ceb4e6d0e3d770
kujino <yorunokuji@gmail.com>
2025-11-28 10:36:53 +0900

    add:reactionに関するアソシエーション,ルーティング生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :posts
+         has_many :reactions, dependent: :destroy
 
          validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
          validates :password, presence: true, on: :create
