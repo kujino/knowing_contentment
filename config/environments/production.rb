d40e340e4567045c46d71f8b0f20ac11fb277d68
kujino <yorunokuji@gmail.com>
2025-12-05 13:56:43 +0900

    fix:デザイン調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://buddhalog.com/' } # 本番環境のURLを入れてください。
+  config.action_mailer.default_url_options = { host: 'buddhalog.com', protocol: 'https' } # 本番環境のURLを入れてください。
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
