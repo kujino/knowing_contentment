6931b0a1d272153f4a123dccad4be90093f957a2
kujino <yorunokuji@gmail.com>
2025-12-19 10:26:14 +0900

    add:Googleログイン実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: :google_oauth2
+  skip_before_action :authenticate_user!
 
   def google_oauth2
     callback_for(:google)
@@ -10,9 +10,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except(:extra)
+      session["devise.google_data"] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
