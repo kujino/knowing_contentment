6a1b6185ba7048919fee29bfdba32bea9ac6acbf
kujino <yorunokuji@gmail.com>
2025-12-25 08:58:11 +0900

    add:pagy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,9 @@ class PostsController < ApplicationController
 
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).includes(:user, :theme, image_attachment: :blob).order(created_at: :desc)
+
+    @pagy,@posts =
+      pagy(@q.result(distinct: true).includes(:user, :theme, image_attachment: :blob).order(created_at: :desc))
   end
 
   def new
